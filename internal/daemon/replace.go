dbfedaee83404735b33c98fd79983ee4faf15478
Mauricio Gomes <mauricio@edge14.com>
2026-07-11 13:43:52 -0400

    Allow Windows state replacement with readers

--- internal/daemon/replace.go
+++ internal/daemon/replace.go
@@ -1,7 +1,15 @@
 package daemon
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
 
 func replaceFile(source, destination string) error {
-	return os.Rename(source, destination)
+	directory, err := os.OpenRoot(filepath.Dir(source))
+	if err != nil {
+		return err
+	}
+	defer directory.Close()
+	return directory.Rename(filepath.Base(source), filepath.Base(destination))
 }
